cb6fc9d8d549690672e5c27d0da23d1c74587297
Kratiux <kakarotiux@gmail.com>
2023-09-20 16:04:53 -0600

    Estructura backend finalizada con conexión de BD

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const app = express();
+
+
+//settings
+app.set('port', process.env.PORT || 4000);
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//routes
+
+module.exports = app;
