d0f03c231527aa82b1a019f77586167c57985b1c
Kratiux <kakarotiux@gmail.com>
2023-11-08 16:32:22 -0600

    Actualizacion

--- backend/src/app.js
+++ backend/src/app.js
@@ -33,7 +33,7 @@ mongoose.set('useCreateIndex', true);
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://deploy-mern-1whq.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true'); // Si necesitas enviar cookies o autenticación
